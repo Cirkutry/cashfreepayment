8950ccd2a2d435abeb90ac47f446d14c063be334
Kre0lidge <149087292+Cirkutry@users.noreply.github.com>
2025-03-20 23:06:19 +0530

    Update messages.php

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -2,4 +2,5 @@
 
 return [
     'info' => 'You will find the App ID and Secret Key in your Cashfree merchant dashboard under the API Keys section.',
+    'currency_info' => 'Cashfree primarily supports INR currency. Other currencies will be converted automatically.',
 ];
